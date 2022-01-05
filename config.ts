6312593f70768ae655cff224005de023a85dc974
yektas <ahmetsercanyektas@gmail.com>
2022-01-06 01:11:24 +0300

    deploy new contract

--- config.ts
+++ config.ts
@@ -1,2 +1,2 @@
-export const nftAddress = "0x0a28cf8B222eE3bfAa63FA8607EFd0AA328B0aBe";
-export const marketAddress = "0x49F4cC37EC8a42502e46aca93DE93CEae5040F41";
+export const nftAddress = "0x37E16f44318E640cCc3f9355D59a09367e99B676";
+export const marketAddress = "0xE0F06F32f6d2E550c3aFc08790530F5e2A02eF80";
